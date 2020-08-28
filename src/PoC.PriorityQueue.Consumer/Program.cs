db91b2419b7c84b406773d84ad8a4430acf0e6ba
Darren Pruitt <darren.pruitt@gmail.com>
2020-08-28 14:36:33 -0500

    Poc masstransit (#4)
    * Added MassTransit PoC
    
    * Updated Nuget packages.
    
    * Updated nuget packages.  things might be broken.
    
    * More updated packages and broken things.
    
    * Adding POC to do priority queues.
    
    * Attempting to set the priority.
    
    * Added consumer for priority poc.
    
    * Reverted RabbitMQ client back to 5.2 to fix build issues.
    
    * Tweaking on the Priority Queue tests.

---
+++ src/PoC.PriorityQueue.Consumer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassTransit;
+using Topshelf;
+
+namespace PoC.PriorityQueue.Consumer
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return (int)HostFactory.Run(cfg => cfg.Service(x => new EventConsumerService()));
+        }
+    }
+
+    class EventConsumerService : ServiceControl
+    {
+        IBusControl _bus;
+
+        public bool Start(HostControl hostControl)
+        {
+            _bus = ConfigureBus();
+            _bus.Start();
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            _bus?.Stop(TimeSpan.FromSeconds(30));
+
+            return true;
+        }
+
+        IBusControl ConfigureBus()
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host("localhost");
+                cfg.PrefetchCount = 2;
+                cfg.EnablePriority(10);
+
+                cfg.ReceiveEndpoint("event_queue", e =>
+                {
+                    e.Handler<IValueEntered>(context =>
+                        Console.Out.WriteLineAsync($"(Priority) Value: ({context.Message.Priority}) {context.Message.Value}"));
+                });
+            });
+        }
+    }
+}
---
+++ src/PoC.PriorityQueue.Producer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace PoC.PriorityQueue.Producer
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host("localhost");
+
+                //cfg.ReceiveEndpoint("test-priority_queue", cep =>
+                //{
+                //    cep.EnablePriority(10);
+                //});
+
+            });
+            
+            // Important! The bus must be started before using it!
+            await busControl.StartAsync();
+            try
+            {
+                var rnd = new Random((int) DateTime.UtcNow.Ticks);
+
+                do
+                {
+                    string value = await Task.Run(() =>
+                    {
+                        Console.WriteLine("Enter number of message (or quit to exit)");
+                        Console.Write("> ");
+                        return Console.ReadLine();
+                    });
+
+                    if ("quit".Equals(value, StringComparison.OrdinalIgnoreCase))
+                        break;
+
+                    int numberOfMessages = int.TryParse(value, out numberOfMessages)
+                        ? numberOfMessages
+                        : 0;
+
+                    int priority = 1;
+                    for (int i = 0; i < numberOfMessages; i++)
+                    {
+                        //priority = priority >= 10 ? 1 : priority + 1;
+                        priority = rnd.Next(1,10);
+                        await busControl.Publish<IValueEntered>(new
+                            {
+                                Value = i,
+                                Priority = priority
+                            },
+                            x =>
+                            {
+                                x.SetPriority((byte) priority);
+                                
+                            });
+                    }
+
+                }
+                while (true);
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+    }
+}
